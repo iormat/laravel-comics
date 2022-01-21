53aac9c47fd929cb369825604740cbf6878c864f
iormat <iorio436@outlook.it>
2022-01-21 16:44:35 +0100

    footer update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,98 @@
 <footer>
-    questo è un messaggio prova del footer
+    <!-- footer up -->
+    <section class="foot_up container-center">
+        <!-- left footer top section -->
+        <div class="footup-left">
+            <section class="footer-nav">
+                <!-- nav dc comics and shop -->
+                <nav>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li>
+                            <a href="#">link footer 1.1</a>
+                            <a href="#">link footer 2.1</a>
+                            <a href="#">link footer 3.1</a>
+                            <a href="#">link footer 4.1</a>
+                            <a href="#">link footer 5.1</a>
+                            <a href="#">link footer 6.1</a>
+                            <a href="#">link footer 7.1</a>
+                        </li>
+                    </ul>
+                    <!-- nav shop -->
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li>
+                            <a href="#">link footer 1.2</a>
+                            <a href="#">link footer 2.2</a>
+                            <a href="#">link footer 3.2</a>
+                            <a href="#">link footer 4.2</a>
+                            <a href="#">link footer 5.2</a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- nav dc -->
+                <nav>
+                    <h3>DC</h3>
+                    <ul>
+                        <li>
+                            <a href="#">link footer 1.3</a>
+                            <a href="#">link footer 2.3</a>
+                            <a href="#">link footer 3.3</a>
+                            <a href="#">link footer 4.3</a>
+                            <a href="#">link footer 5.3</a>
+                            <a href="#">link footer 6.3</a>
+                            <a href="#">link footer 7.3</a>
+                            <a href="#">link footer 8.3</a>
+                            <a href="#">link footer 9.3</a>
+                            <a href="#">link footer 10.3</a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- nav sites -->
+                <nav>
+                    <h3>SITES</h3>
+                    <ul>
+                        <li>
+                            <a href="#">link footer 1.4</a>
+                            <a href="#">link footer 2.4</a>
+                            <a href="#">link footer 3.4</a>
+                            <a href="#">link footer 4.4</a>
+                            <a href="#">link footer 5.4</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+        <!-- right footer top section -->
+        <div class="footup-right">
+        </div>
+    </section>
+    <!-- footer down -->
+    <section class="foot_down">
+        <div class="container-center">
+            <button class="btn footer-btn">
+                <a href="#"> SIGN&minus;IN NOW&excl; </a>
+            </button>
+            <!-- social footer section -->
+            <section class="social">
+                <h3>FOLLOW US</h3>
+                <!-- social links icons -->
+                <a href="#">
+                    <img src="{{asset('img/footer-facebook.png')}}" alt="facebook logo">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-twitter.png')}}" alt="facebook logo">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-youtube.png')}}" alt="facebook logo">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-pinterest.png')}}" alt="facebook logo">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-periscope.png')}}" alt="facebook logo">
+                </a>
+            </section>
+        </div>
+    </section>
 </footer>
