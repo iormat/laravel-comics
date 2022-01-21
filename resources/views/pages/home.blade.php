fb094472de3a53a4ad9713b3aa11254f19083329
iormat <iorio436@outlook.it>
2022-01-21 17:37:34 +0100

    pages.home update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,181 @@
 
 @section('content')
     <main>
-        <h2>
-            Hello World from Home
-        </h2>
-        <a href="{{route('test')}}">GO TO TEST PAGE</a>
+        <!-- section comics -->
+        <section id="comics">
+            <h2>current series</h2>
+            <section id="comics-cards" class="container-center">
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ" alt="Action Comics">
+                            </div>
+                            <h3>Action Comics</h3>
+                            <p>$19.99</p>
+                        </a>
+                    </div>                
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="American Vampire 1976">
+                            </div>
+                            <h3>American Vampire 1976</h3>
+                            <p>$3.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01" alt="Aquaman">
+                            </div>
+                            <h3>Aquaman</h3>
+                            <p>$16.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb" alt="Batgirl">
+                            </div>
+                            <h3>Batgirl</h3>
+                            <p>$2.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76" alt="Batman">
+                            </div>
+                            <h3>Batman</h3>
+                            <p>$3.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ" alt="Batman Beyond">
+                            </div>
+                            <h3>Batman Beyond</h3>
+                            <p>$2.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8" alt="Batman/Superman">
+                            </div>
+                            <h3>Batman/Superman</h3>
+                            <p>$3.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk" alt="Batman/Superman Annual">
+                            </div>
+                            <h3>Batman/Superman Annual</h3>
+                            <p>$4.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8" alt="Batman: The Joker War Zone">
+                            </div>
+                            <h3>Batman: The Joker War Zone</h3>
+                            <p>$$5.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY" alt="Batman: Three Jokers">
+                            </div>
+                            <h3>Batman: Three Jokers</h3>
+                            <p>$6.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX" alt="Batman: White Knight Presents: Harley Quinn">
+                            </div>
+                            <h3>Batman: White Knight Presents: Harley Quinn</h3>
+                            <p>$4.99</p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <a href="#">
+                            <div class="img-window">
+                                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq" alt="Catwoman">
+                            </div>
+                            <h3>Catwoman</h3>
+                            <p>$16.99</p>
+                        </a>
+                    </div>
+                </li>
+            </section>
+            <div class="see-more">
+                <button> <a href="@">load more</a> </button>
+            </div>
+        </section>
+        <!-- section cards -->
+        <section id="link-cards">
+            <ul class="container-center">
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="digital comicse">
+                        <p>digital comics</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="subscription">
+                        <p>subscription</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="comic shop locator">
+                        <p>comic shop locator</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="dc power visa">
+                        <p>dc power visa</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="digital comicse">
+                        <p>digital comics</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
     </main>
 @endsection
