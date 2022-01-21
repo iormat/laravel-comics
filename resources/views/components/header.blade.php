1949c0e3664b158af55d94d276eacd386c721d6d
iormat <iorio436@outlook.it>
2022-01-21 16:24:52 +0100

    header update

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,24 @@
 <header>
-    questo è un messaggio prova dell'header
+    <div class="top-bar">
+        <div class="container-center">
+            <img src="{{asset('img/dc-logo.png')}}" alt="">
+            <section class="header-nav">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">prova 1</a></li>
+                        <li>
+                            <a href="#">prova 2</a></li>
+                        <li>
+                            <a href="#">prova 3</a></li>
+                        <li>
+                            <a href="#">prova 4</a></li>
+                        <li>
+                            <a href="#">prova 5</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    </div>
+    <section id="jumbo"></section>
 </header>
